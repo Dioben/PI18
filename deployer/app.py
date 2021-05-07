79d7aadeaf48fa49b1798a3d0b271a9f3af821de
pmasilva20 <pmasilva20@ua.pt>
2021-05-07 05:48:06 +0100

    Setup deployer and done most of the work necessary
    While docker operations worked in isolation, I had problems
    trying to connect to the Flask server and Redis broker it uses,
    both of this seem to be problems related to how docker exposes
    ports.

--- deployer/app.py
+++ deployer/app.py
@@ -1,16 +1,25 @@
 from flask import Flask, request
 from celery import Celery
+import docker
+import uuid
 
 app = Flask(__name__)
+client = docker.from_env()
 
 @app.route('/simulations', methods=['POST'])
 def make_simulation():
-    data = request.get_json()
-
-    result = make_simualtion.delay(model_data,dataset_data,conf_data)
-    result.wait() 
-    #Should return the simulation ID
-    return 'All good'
+    try:
+        data = request.get_json()
+        print('data')
+        id_gen = uuid.uuid4()
+        sim_id = id_gen.int
+        result = make_simualtion.delay(sim_id)
+        result.wait()
+        #Should return the simulation ID
+        return sim_id
+    except Exception as e:
+        print('ERROR:',e)
+    return None
 
 @app.route('/simulations/<simulation_id>', methods=['DELETE'])
 def delete_simulation(simulation_id):
@@ -36,8 +45,8 @@ def make_celery(app):
     return celery
 
 app.config.update(
-    broker_url = 'redis://localhost:6379',
-    result_backend = 'redis://localhost:6379',
+    broker_url = 'redis://localhost:6380',
+    result_backend = 'redis://localhost:6380',
 
     task_serializer = 'json',
     result_serializer = 'json',
@@ -48,16 +57,35 @@ app.config.update(
 celery = make_celery(app)
 
 @celery.task()
-def make_simualtion(model_data,dataset_data,conf_data):
-    #Placeholder
+def make_simualtion(sim_id):
+    try:
+        container_made = client.containers.run("dioben/nntrackerua-simulation",name=str(sim_id),detach=True)
+        print('Containner with id:'+container_made.id + " was made")
+    except Exception as e:
+        print('ERROR:',e)
     return None
 
 @celery.task()
 def delete_simulation(simulation_id):
-    #Placeholder
+    try:
+        containner_kill = client.containers.get(simulation_id)
+        containner_kill.kill()
+    except Exception as e:
+        print('ERROR:',e)
     return None
 
 @celery.task()
 def change_simulation(simulation_id,command):
-    #Placeholder
+    try:
+        containner = client.containers.get(simulation_id)
+        if 'START' in command:
+            containner.unpause()
+        elif 'PAUSE' in command:
+            containner.pause()
+        elif 'STOP' in command:
+            containner.stop()
+        else:
+            print('ERROR:','Given invalid command ',command)
+    except Exception as e:
+        print('ERROR:',e)
     return None
