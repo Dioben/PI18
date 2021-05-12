a4738f286b16f6e0f0b4686f05ca291c5f64ebb1
buckaroo69 <diogobento@ua.pt>
2021-05-12 22:54:25 +0100

    django changes

--- server/app/views.py
+++ server/app/views.py
@@ -5,9 +5,10 @@ from django.shortcuts import render, redirect
 from django.http import HttpResponseRedirect, HttpResponse
 from django.shortcuts import render
 from django.views.decorators.csrf import csrf_exempt
-
-from .forms import  UploadModelFileForm, UploadDataSetFileForm
+import requests
+from .forms import UploadModelFileForm, UploadDataSetFileForm
 from app.models import *
+
 
 def index(request):
     return render(request, 'index.html')
@@ -37,37 +38,48 @@ def simulation_info(request, id):
 # from somewhere import handle_uploaded_file
 
 
-
-
-def post_sim(request): #probably add a 3rd form to this thing
+def post_sim(request):  # probably add a 3rd form to this thing
+    #TODO: ENABLE SENDING URLS INSTEAD OF FILES AS WELL
     form1 = UploadModelFileForm(request.POST, request.FILES)
     form2 = UploadDataSetFileForm(request.POST, request.FILES)
     if form1.is_valid() and form2.is_valid():
-        print("Got Model")
         # handle_uploaded_file(request.FILES['model'])
         model = request.FILES['model']
         path_model = model.temporary_file_path()
         print(path_model)
-
-        print("Got Dataset")
         # handle_uploaded_file(request.FILES['dataset'])
         dataset = request.FILES['dataset']
         path_dataset = dataset.temporary_file_path()
         print(str(path_dataset))
-        # do something
-        print("Simulation Created and Started")
-        # return HttpResponseRedirect('/success/url/')
-        return redirect('Simulations') #TODO: REPLACE THIS WITH CREATED SIMULATION
+        # TODO: read model into db, count layers into db, copy raw bias bytes, epoch interval, name, goal epochs
+        sim = Simulation(owner=request.user, isdone=False, isrunning=False, model="read model file onto this i guess?", name="sample text",
+                         biases=b'sample text', epoch_interval=10, goal_epochs=100)
+        sim.save()
+        #TODO: DISTINCT TRAIN AND TEST DATASETS MAYBE
+        postdata = {"conf": {"id": sim.id,
+                             "dataset_train": path_dataset,
+                             "dataset_test": path_dataset,
+                             "dataset_url": True,
+                             "epochs": sim.goal_epochs,
+                             "interval": sim.epoch_interval},
+                    "model": {sim.model}
+                    }
+        resp = requests.post("tracker-deployer:7000/simulations", postdata)
+        if resp.ok:
+            return sim
+        sim.delete()
+        return HttpResponse("Failed to reach deployer", 500)
 
 
 @csrf_exempt
 def simulations(request):
-    if not request.user.is_authenticated: #you could use is_active here for email verification i think
+    if not request.user.is_authenticated:  # you could use is_active here for email verification i think
         return HttpResponse("Please Log In", 403)
     if request.method == 'POST':
         return post_sim(request)
     else:
         return Simulation.objects.filter(owner=request.user)
+
 
 @csrf_exempt
 def get_simulation(request, id):
@@ -79,30 +91,44 @@ def get_simulation(request, id):
     if sim.owner == request.user:
         if request.method == "DELETE":
             sim.delete()
-            return HttpResponse(sim,200)
+            return HttpResponse(sim, 200)
         return sim
     else:
         return HttpResponse("Forbidden", 403)
 
 
-def command_start(request, id): #return the objects you're acting on in these
-    pass
-
+def command_start(request, id):  # return the objects you're acting on in these
+    sim = Simulation.objects.get(id)
+    requests.post(f'tracker-deployer:7000/simulations/{id}/START')
+    sim.isrunning = True
+    return HttpResponse(sim,200)
 
 def command_stop(request, id):
-    pass
+    sim = Simulation.objects.get(id)
+    requests.delete(f'tracker-deployer:7000/simulations/{id}')
+    sim.delete()
+    return HttpResponse(sim, 200)
+
+
+def command_pause(request, id):
+    sim = Simulation.objects.get(id)
+    requests.post(f'tracker-deployer:7000/simulations/{id}/PAUSE')
+    sim.isrunning = False
+    sim.save()
+    return HttpResponse(sim, 200)
 
 
 @csrf_exempt
 def command_simulation(request, command, id):
-    if request.method == 'POST':
-        if command == "START":
-            return command_start(request,id)
-        elif command == "STOP":
-            return command_stop(request,id)
-        else:
-            # Do Something
-            return None
+    if not request.user.is_authenticated:
+        return HttpResponse("Please log in",403)
+    if not Simulation.objects.filter(id__exact=id,owner=request.user).exists():
+        return HttpResponse("You do not own this simulation",403)
+    if command == "START":
+        return command_start(request, id)
+    elif command == "STOP":
+        return command_stop(request, id)
+    elif command == "PAUSE":
+        return command_pause(request, id)
     else:
-        # Do Something
-        return None
+        return HttpResponse("Unknown command", 400)
