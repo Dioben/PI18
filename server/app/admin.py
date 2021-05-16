411ce336ceffecfd05bf0d1cc4b7eac271557490
JoaoTS20 <joaots@ua.pt>
2021-05-16 16:41:07 +0100

    Fixed Progress Bar

--- server/app/admin.py
+++ server/app/admin.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
 
 # Register your models here.
-from app.models import Simulation
+from app.models import Simulation, Update
 
 admin.site.register(Simulation)
+admin.site.register(Update)
+
 
