919c7364981cc0ee60bfa83dd7f126b56ba8b624
p-laranjinha <plcasimiro@ua.pt>
2021-06-11 04:53:01 +0100

    Environment variables for possible allowed hosts

--- server/NeuralNetTracker/settings.py
+++ server/NeuralNetTracker/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '!og!1a3%qkr8wss2rg-h$z*$$6n7&z(l4r=+b$_m-kkk-!5urn'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['localhost', '127.0.0.1', '10.0.12.67']
+ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS').split(',')
 
 
 # Application definition
